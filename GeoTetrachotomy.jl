2c446622e8ff1eaf923778941c485ae1d174daba
t-garin <t.garin@proton.me>
2025-01-12 23:26:10 +0100

    Update GeoTetrachotomy.jl

--- GeoTetrachotomy.jl
+++ GeoTetrachotomy.jl
@@ -1,16 +1,28 @@
 using BenchmarkTools
 using DataStructures: DiBitVector
 
-"geopoint representation using tetrachotomy"
 struct Tetra
     # DiBitVector to represent the tetrachotomy
     dbv::DiBitVector
 end
 
+struct TetraBV
+    lat::BitVector
+    lon::BitVector
+end
+
+struct TetraVB
+    lat::Vector{Bool}
+    lon::Vector{Bool}
+end
+
 "geopoint representation using latlon"
 struct LatLon
     # latitude
-    lat::Float
+    lat::Number
     # longitude
-    lon::Float
+    lon::Number
 end
+
+ll = LatLon(Float64(0), Float64(0))
+print("Size of ll: ", sizeof(ll), " bytes")
