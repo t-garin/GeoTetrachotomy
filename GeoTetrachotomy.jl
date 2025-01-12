06baacb5ac2fead4988d862d13d0e12aae627612
t-garin <t.garin@proton.me>
2025-01-13 00:40:57 +0100

    Update GeoTetrachotomy.jl

--- GeoTetrachotomy.jl
+++ GeoTetrachotomy.jl
@@ -51,11 +51,39 @@ function _get_dicho(value::Number, n::Int, lmin::Number, lmax::Number)
             lmin = lmid
         end 
     end
-    return dicho, (lmin, lmax)
+    return dicho, abs(lmax - lmin)
 end
 
 latdicho(value:: Number, n:: Int) = _get_dicho(value, n,  -90,  90)
 londicho(value:: Number, n:: Int) = _get_dicho(value, n, -180, 180)
 
-randlat(n::Int) = (rand(n).-0.5) * 180
-randlon(n::Int) = (rand(n).-0.5) * 360
+randlat(n::Int)::Vector{Float64} = (rand(n).-0.5) * 180
+randlon(n::Int)::Vector{Float64} = (rand(n).-0.5) * 360
+
+function dicho(ll::LatLon, precision::Int)::TetraBV
+    return TetraBV(
+        latdicho(ll.lat, precision)[1],
+        londicho(ll.lon, precision)[1]
+    )
+end
+
+function delta(ll::LatLon, precision::Int)
+    return (latdicho(ll.lat, precision)[2], londicho(ll.lon, precision)[2])
+end
+function test(npoints::Int, precision::Int)
+    latlon = [LatLon(lat, lon) for (lat, lon) in zip(randlat(npoints), randlon(npoints))]
+    tetras = [dicho(ll, precision) for ll in latlon]
+    deltas = [delta(ll, precision) for ll in latlon]
+    
+    pprint(ll::LatLon) = print(sizeof(ll.lat), " & ", sizeof(ll.lon), " bytes")
+    pprint(tt::TetraBV) = print(sizeof(tt.lat), " & ", sizeof(tt.lon), " bytes")
+
+    for (ll, tt, ds) in zip(latlon, tetras, deltas)
+        @show ll.lat, ll.lon, ds
+        pprint(ll)
+        print('\t')
+        pprint(tt)
+        print('\n')
+    end
+    # return tetras#, deltas
+end
